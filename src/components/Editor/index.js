33ee07c95466ab5992cf18b002b60787489ee674
jiangtao <jtaobox@126.com>
2020-07-02 00:22:13 +0800

    add postmessage

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.less";
 
-const Editor = () => {
+const Editor = ({ value, onChange }) => {
+  const [taValue, setTaValue] = useState(value);
+
+  useEffect(() => {
+    setTaValue(value);
+  }, [value]);
+
+  const _onChange = (value) => {
+    if (onChange) {
+      onChange(value);
+    } else {
+      setTaValue(value);
+    }
+  };
 
   return (
     <div className="editor">
-      <textarea placeholder="Enter something funny" type="text"></textarea>
+      <textarea
+        placeholder="Enter something funny"
+        type="text"
+        value={taValue}
+        onChange={_onChange}
+      ></textarea>
     </div>
-  )
+  );
 };
-
 
 export default Editor;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import Editor from "./components/Editor";
+import { instance as poster } from "./postMessage";
 
 import "./index.less";
 
 const App = () => {
+  const [text, setText] = useState("");
+
+  const setContent = () => {
+    setText("hello")
+  }
+
+  useEffect(() => {
+    poster.subscribe("vscBaseMarkdown.setContent", setContent)
+  }, [])
+
   return (
-    <div className="app"><Editor /></div>
-  )
+    <div className="app">
+      <Editor value={text} />
+    </div>
+  );
 };
 
-
-ReactDom.render(<App />, document.getElementById("app"))
+ReactDom.render(<App />, document.getElementById("app"));
